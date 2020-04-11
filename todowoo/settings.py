099105a047df4b2de20b37c7a7f6266b22b8b2f2
rileylitchfield <rileylitchfield95@gmail.com>
2020-04-10 22:17:44 -0500

    test

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', cast=bool)
+DEBUG=False
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
